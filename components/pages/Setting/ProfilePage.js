ed7212113cb8b5ac9867c92cb5a236ddd4b63159
Amd2D <amdelcheikh@gmail.com>
2021-05-07 05:07:57 +0300

    profile, edit profile and password change fixes

---
+++ components/pages/Setting/ProfilePage.js
@@ -0,0 +1,214 @@
+import React from "react";
+import { StyleSheet, View, Image, SafeAreaView } from "react-native";
+import {
+    Avatar,
+    Title,
+    Caption,
+    Text,
+    TouchableRipple,
+} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import OtherIcon from "react-native-vector-icons/Ionicons";
+import {useDispatch} from "react-redux";
+import { loggedOut } from "../../../actions/action";
+import { Login } from "../../Login/LoginPage";
+
+export default function Profile({ navigation }) {
+    const dispatch = useDispatch();
+
+    function signOut() {
+        const data = {
+            id: 1,
+        };
+        dispatch(loggedOut(data));
+        console.log("SIGN OUT");
+        return <Login/>;
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.userInfoSection}>
+                <View style={{flexDirection: 'row', marginTop: 15}}>
+                    <Avatar.Image
+                        source={{
+                            uri: "https://bootdey.com/img/Content/avatar/avatar6.png",
+                        }}
+                        size={80}
+                    />
+                    <View style={{marginLeft: 20}}>
+                        <Title style={[styles.title, {
+                            marginTop: 15,
+                            marginBottom: 5,
+                        }]}>Ahmad EC</Title>
+                        <Caption style={styles.caption}>@username</Caption>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.userInfoSection}>
+                <View style={styles.row}>
+                    <Icon name="map-marker-radius" color="#777777" size={20}/>
+                    <Text style={{color: "#777777", marginLeft: 20}}>Address</Text>
+                </View>
+                <View style={styles.row}>
+                    <Icon name="phone" color="#777777" size={20}/>
+                    <Text style={{color: "#777777", marginLeft: 20}}>cell</Text>
+                </View>
+                <View style={styles.row}>
+                    <Icon name="email" color="#777777" size={20}/>
+                    <Text style={{color: "#777777", marginLeft: 20}}>email</Text>
+                </View>
+            </View>
+            <View style={styles.infoBoxWrapper}>
+                <View style={[styles.infoBox, {
+                    borderRightColor: '#dddddd',
+                    borderRightWidth: 1
+                }]}>
+                    <Title>22</Title>
+                    <Caption>Age</Caption>
+                </View>
+                <View style={styles.infoBox}>
+                    <Title>B-</Title>
+                    <Caption>Blood Type</Caption>
+                </View>
+            </View>
+            <View style={styles.infoBoxWrapper}>
+                <View style={[styles.infoBox, {
+                    borderRightColor: '#dddddd',
+                    borderRightWidth: 1
+                }]}>
+                    <Title>170 cm</Title>
+                    <Caption>Height</Caption>
+                </View>
+                <View style={styles.infoBox}>
+                    <Title>70 kg</Title>
+                    <Caption>Weight</Caption>
+                </View>
+            </View>
+            <View style={styles.menuWrapper}>
+                <TouchableRipple onPress={() => navigation.navigate("EditPage")}>
+                    <View style={styles.menuItem}>
+                        <Icon name="account-edit" color="#1498D5" size={25}/>
+                        <Text style={styles.menuItemText}>Edit Profile</Text>
+                    </View>
+                </TouchableRipple>
+                <TouchableRipple onPress={() => navigation.navigate("ChangePass")}>
+                    <View style={styles.menuItem}>
+                        <Icon name="lock-open-outline" color="#1498D5" size={25}/>
+                        <Text style={styles.menuItemText}>Change Password</Text>
+                    </View>
+                </TouchableRipple>
+                <TouchableRipple onPress={() => signOut()}>
+                    <View style={styles.menuItem}>
+                        <OtherIcon name="log-out-outline" color="#1498D5" size={25}/>
+                        <Text style={styles.menuItemText}>Log Out</Text>
+                    </View>
+                </TouchableRipple>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    userInfoSection: {
+        paddingHorizontal: 30,
+        marginBottom: 25,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    caption: {
+        fontSize: 14,
+        lineHeight: 14,
+        fontWeight: '500',
+    },
+    row: {
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
+    infoBoxWrapper: {
+        borderBottomColor: '#dddddd',
+        borderBottomWidth: 1,
+        borderTopColor: '#dddddd',
+        borderTopWidth: 1,
+        flexDirection: 'row',
+        height: 100,
+    },
+    infoBox: {
+        width: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    menuWrapper: {
+        marginTop: 10,
+    },
+    menuItem: {
+        flexDirection: 'row',
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+    },
+    menuItemText: {
+        color: '#777777',
+        marginLeft: 20,
+        fontWeight: '600',
+        fontSize: 16,
+        lineHeight: 26,
+    },
+
+    header: {
+        backgroundColor: "#DCDCDC",
+    },
+    headerContent: {
+        padding: 30,
+        alignItems: "center",
+    },
+    avatar: {
+        width: 130,
+        height: 130,
+        borderRadius: 63,
+        borderWidth: 4,
+        borderColor: "white",
+        marginBottom: 10,
+    },
+    name: {
+        fontSize: 22,
+        color: "#000000",
+        fontWeight: "600",
+    },
+    userInfo: {
+        fontSize: 16,
+        color: "#778899",
+        fontWeight: "600",
+    },
+    body: {
+        backgroundColor: "#778899",
+        height: 500,
+        alignItems: "center",
+    },
+    item: {
+        flexDirection: "row",
+    },
+    infoContent: {
+        flex: 1,
+        alignItems: "flex-start",
+        paddingLeft: 5,
+    },
+    iconContent: {
+        flex: 1,
+        alignItems: "flex-end",
+        paddingRight: 5,
+    },
+    icon: {
+        width: 30,
+        height: 30,
+        marginTop: 20,
+    },
+    info: {
+        fontSize: 18,
+        marginTop: 20,
+        color: "#FFFFFF",
+    },
+});

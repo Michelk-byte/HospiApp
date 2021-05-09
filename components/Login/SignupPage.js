fae2bf06fe1447a531a9f9438113649701666932
Amd2D <amdelcheikh@gmail.com>
2021-05-09 12:51:48 +0300

    sign up page

--- components/Login/SignupPage.js
+++ components/Login/SignupPage.js
@@ -3,19 +3,16 @@ import {
   StyleSheet,
   Text,
   View,
-  StatusBar,
   TextInput,
   TouchableOpacity,
   ImageBackground,
-    Platform
+  Platform
 } from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
+import * as Animatable from "react-native-animatable";
 import { useDispatch, useSelector } from "react-redux";
 import { signUp, register } from "../../actions/action";
-import { Picker } from '@react-native-picker/picker'
-import Slider from '@react-native-community/slider';
-
-import Login from "./LoginPage";
-
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 export default function Signup({ navigation }) {
   const [username, setUName] = useState("");
@@ -65,113 +62,155 @@ export default function Signup({ navigation }) {
 
   return (
       <View style={styles.container}>
-        <ImageBackground source={require('../../assets/SignupBG.jpg')} style={styles.image} blurRadius={Platform.OS=="ios"? 30:3}>
-        <View style={styles.container}>
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="Username"
-            placeholderTextColor="#ffffff"
-            selectionColor="#fff"
-            keyboardType="email-address"
-            onChangeText={(user) => setUName(user)}
-          />
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="Email"
-            placeholderTextColor="#ffffff"
-            selectionColor="#fff"
-            keyboardType="email-address"
-            onChangeText={(email) => SetEmail(email)}
-          />
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="Password"
-            secureTextEntry={true}
-            placeholderTextColor="#ffffff"
-            onChangeText={(pass) => SetPassword(pass)}
-          />
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="Verify Password"
-            secureTextEntry={true}
-            placeholderTextColor="#ffffff"
-            onChangeText={(verifypass) => SetVerifypass(verifypass)}
-          />
-          <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="PhoneNumber"
-            placeholderTextColor="#ffffff"
-            selectionColor="#fff"
-            keyboardType=""
-            onChangeText={(pnumber) => SetPnumber(pnumber)}
-          />
-           <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="Weight Kg"
+        <ImageBackground source={require('../../assets/SignupBG.jpg')} style={styles.image} blurRadius={Platform.OS=="ios"? 10:1}>
+          <Animatable.View animation="fadeInDownBig" style={styles.box}>
+            <Text style={styles.title}>Create Account</Text>
+            <Text style={styles.header}>Create your account. It's free and only takes a couple of minutes</Text>
+            <View style={styles.input}>
+              <TextInput
+                  style={styles.textInput}
+                  placeholder="Username"
+                  placeholderTextColor="#003f5c"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                  style={styles.textInput}
+                  placeholder="First Name"
+                  placeholderTextColor="#003f5c"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+              />
+              <TextInput
+                  style={styles.textInput}
+                  placeholder="Last Name"
+                  placeholderTextColor="#003f5c"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                  style={styles.textInput}
+                  placeholder="Email"
+                  keyboardType={"email-address"}
+                  placeholderTextColor="#003f5c"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                  style={styles.textInput}
+                  placeholder="Password"
+                  placeholderTextColor="#003f5c"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                  style={styles.textInput}
+                  placeholder="Verify Password"
+                  placeholderTextColor="#003f5c"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+              />
+            </View>
+            <View>
+              <TouchableOpacity onPress={()=>navigation.navigate('LoginScreen')}>
+                <LinearGradient colors={["#37c9fc", "#1498D5"]} style={styles.button}>
+                  <Text style={styles.buttonText}>Next</Text>
+                  <MaterialIcons name="navigate-next" color='white' size={20}/>
+                </LinearGradient>
+              </TouchableOpacity>
+            </View>
+            {/*<TextInput*/}
+            {/*    style={styles.inputBox}*/}
+            {/*    underlineColorAndroid="rgba(0,0,0,0)"*/}
+            {/*    placeholder="Email"*/}
+            {/*    placeholderTextColor="#ffffff"*/}
+            {/*    selectionColor="#fff"*/}
+            {/*    keyboardType="email-address"*/}
+            {/*    onChangeText={(email) => SetEmail(email)}*/}
+            {/*/>*/}
+            {/*<TextInput*/}
+            {/*    style={styles.inputBox}*/}
+            {/*    underlineColorAndroid="rgba(0,0,0,0)"*/}
+            {/*    placeholder="Password"*/}
+            {/*    secureTextEntry={true}*/}
+            {/*    placeholderTextColor="#ffffff"*/}
+            {/*    onChangeText={(pass) => SetPassword(pass)}*/}
+            {/*/>*/}
+            {/*<TextInput*/}
+            {/*    style={styles.inputBox}*/}
+            {/*    underlineColorAndroid="rgba(0,0,0,0)"*/}
+            {/*    placeholder="Verify Password"*/}
+            {/*    secureTextEntry={true}*/}
+            {/*    placeholderTextColor="#ffffff"*/}
+            {/*    onChangeText={(verifypass) => SetVerifypass(verifypass)}*/}
+            {/*/>*/}
+            {/*<TextInput*/}
+            {/*    style={styles.inputBox}*/}
+            {/*    underlineColorAndroid="rgba(0,0,0,0)"*/}
+            {/*    placeholder="PhoneNumber"*/}
+            {/*    placeholderTextColor="#ffffff"*/}
+            {/*    selectionColor="#fff"*/}
+            {/*    keyboardType=""*/}
+            {/*    onChangeText={(pnumber) => SetPnumber(pnumber)}*/}
+            {/*/>*/}
+            {/*<TextInput*/}
+            {/*    style={styles.inputBox}*/}
+            {/*    underlineColorAndroid="rgba(0,0,0,0)"*/}
+            {/*    placeholder="Weight Kg"*/}
 
-            placeholderTextColor="#ffffff"
-            onChangeText={(w) => setWeight(w)}
-          />
-            <TextInput
-            style={styles.inputBox}
-            underlineColorAndroid="rgba(0,0,0,0)"
-            placeholder="height cm"
+            {/*    placeholderTextColor="#ffffff"*/}
+            {/*    onChangeText={(w) => setWeight(w)}*/}
+            {/*/>*/}
+            {/*<TextInput*/}
+            {/*    style={styles.inputBox}*/}
+            {/*    underlineColorAndroid="rgba(0,0,0,0)"*/}
+            {/*    placeholder="height cm"*/}
 
-            placeholderTextColor="#ffffff"
-            onChangeText={(w) => setHeight(w)}
-          />
-
-<Picker style={{ marginTop:10,}}
-          selectedValue={blood}
-          onValueChange={currentBlood => setBlood(currentBlood)}>
-          <Picker.Item label="A+" value="A+" />
-          <Picker.Item label="A-" value="A-" />
-          <Picker.Item label="B+" value="B+" />
-          <Picker.Item label="B-" value="B-" />
-          <Picker.Item label="O+" value="O+" />
-          <Picker.Item label="O-" value="O-" />
-          <Picker.Item label="AB+" value="AB+" />
-          <Picker.Item label="AB-" value="AB-" />
-        </Picker>
-        <Text
-          style={{
-            fontSize: 30,
-            color: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          Blood type: {blood}
-        </Text>
-
-
-
-
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText} onPress={() => handleS()}>
-              Register
-            </Text>
-          </TouchableOpacity>
-          <Text style={{ color: { messagecolor } }}>{message_show}</Text>
-        </View>
-
-        <View style={styles.signupTextCont}>
-          <Text style={styles.signupText}>Already have an account?</Text>
-          <TouchableOpacity>
-            <Text
-              style={styles.signupButton}
-              onPress={() => navigation.navigate("LoginScreen0")}
-            >
-              {" "}
-              Sign in
-            </Text>
-          </TouchableOpacity>
-        </View>
+            {/*    placeholderTextColor="#ffffff"*/}
+            {/*    onChangeText={(w) => setHeight(w)}*/}
+            {/*/>*/}
+            {/*<Picker style={{ marginTop:10,}}*/}
+            {/*        selectedValue={blood}*/}
+            {/*        onValueChange={currentBlood => setBlood(currentBlood)}>*/}
+            {/*  <Picker.Item label="A+" value="A+" />*/}
+            {/*  <Picker.Item label="A-" value="A-" />*/}
+            {/*  <Picker.Item label="B+" value="B+" />*/}
+            {/*  <Picker.Item label="B-" value="B-" />*/}
+            {/*  <Picker.Item label="O+" value="O+" />*/}
+            {/*  <Picker.Item label="O-" value="O-" />*/}
+            {/*  <Picker.Item label="AB+" value="AB+" />*/}
+            {/*  <Picker.Item label="AB-" value="AB-" />*/}
+            {/*</Picker>*/}
+            {/*<Text*/}
+            {/*    style={{*/}
+            {/*      fontSize: 30,*/}
+            {/*      color: '#fff',*/}
+            {/*      alignItems: 'center',*/}
+            {/*      justifyContent: 'center',*/}
+            {/*    }}>*/}
+            {/*  Blood type: {blood}*/}
+            {/*</Text>*/}
+            {/*<TouchableOpacity style={styles.button}>*/}
+            {/*  <Text style={styles.buttonText} onPress={() => handleS()}>*/}
+            {/*    Register*/}
+            {/*  </Text>*/}
+            {/*</TouchableOpacity>*/}
+            {/*<Text style={{ color: { messagecolor } }}>{message_show}</Text>*/}
+          </Animatable.View>
+          <View style={styles.logIn}>
+            <Text style={styles.logText}>Already have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("LoginScreen")}>
+              <Text style={styles.signIn}>Sign in</Text>
+            </TouchableOpacity>
+          </View>
         </ImageBackground>
       </View>
   );
@@ -187,48 +226,63 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
-  inputBox: {
-    width: 300,
-    height: 30,
-    backgroundColor: "rgba(255, 255,255,0.2)",
-    borderRadius: 25,
-    paddingHorizontal: 16,
-    fontSize: 16,
-    color: "#ffffff",
-    marginVertical: 13,
+  box:{
+    borderWidth:2,
+    borderColor:'#1498D5',
+    borderRadius:15,
+    backgroundColor:'white',
+    height:600,
+    width:350,
+    alignSelf:'center',
+    alignItems:'center'
   },
-  button: {
-    width: 300,
-    backgroundColor: "#1c313a",
-    borderRadius: 25,
-    marginVertical: 10,
-    paddingVertical: 13,
+  title:{
+    fontSize: 30,
+    marginBottom: 10
+  },
+  header:{
+    fontSize: 11,
+    marginBottom: 50,
+    color:'gray'
+  },
+  input:{
+    width:'80%',
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+    paddingBottom: 2,
+    marginBottom: 50,
+  },
+  textInput:{
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    paddingLeft: 10,
+    color: "black",
+  },
+  button:{
+    marginTop:10,
+    width: 100,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    flexDirection: 'row'
   },
   buttonText: {
-    fontSize: 16,
-    fontWeight: "500",
-    color: "#ffffff",
-    textAlign: "center",
+    color: 'white',
+    fontWeight: 'bold',
+    paddingRight:5
   },
-  container1: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  logIn:{
+    marginTop:20
   },
-  signupTextCont: {
-    flexGrow: 1,
-    alignItems: "flex-end",
-    justifyContent: "center",
-    paddingVertical: 16,
-    flexDirection: "row",
+  logText:{
+    alignSelf:'center',
   },
-  signupText: {
-    color: "rgba(255,255,255,0.6)",
-    fontSize: 16,
-  },
-  signupButton: {
-    color: "#ffffff",
-    fontSize: 16,
-    fontWeight: "500",
+  signIn:{
+    marginTop:10,
+    alignSelf:'center',
+    fontSize:20,
   },
 });

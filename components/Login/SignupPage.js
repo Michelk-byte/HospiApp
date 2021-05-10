78495c1ad6c05ae286d994767b978036e601a55d
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-05-11 00:07:41 +0300

    add alert signup

--- components/Login/SignupPage.js
+++ components/Login/SignupPage.js
@@ -6,23 +6,23 @@ import {
   TextInput,
   TouchableOpacity,
   ImageBackground,
-  Platform
+  Platform,
 } from "react-native";
-import {LinearGradient} from "expo-linear-gradient";
+import { LinearGradient } from "expo-linear-gradient";
 import * as Animatable from "react-native-animatable";
 import { useDispatch, useSelector } from "react-redux";
 import { signUp, register } from "../../actions/action";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import {Button} from "react-native-elements";
+import { Button } from "react-native-elements";
 
 export default function Signup({ navigation }) {
   const [username, setUName] = useState("");
   const [Email, SetEmail] = useState("");
   const [fname, SetFname] = useState("");
   const [lname, SetLname] = useState("");
-  const [pass,setPass]=useState("");
-  const [vPass,setVpass]=useState("");
+  const [pass, setPass] = useState("");
+  const [vPass, setVpass] = useState("");
 
   const dispatch = useDispatch();
 
@@ -34,146 +34,149 @@ export default function Signup({ navigation }) {
   let register_ = useSelector((state) => state.Signup.register);
 
   if (register_) {
-    SetMEssageShow(message);
-    if (status === 200) {
-      console.log("green");
-      SetMessageColor("green");
-    } else if (status !== 200 && status !== "") {
-      console.log("red");
-      console.log(message);
-      SetMessageColor("red");
-    }
+    alert(message);
     dispatch(register(false));
   }
 
-
-
   return (
-      <View style={styles.container}>
-        <ImageBackground source={require('../../assets/SignupBG.jpg')} style={styles.image} blurRadius={Platform.OS=="ios"? 10:1}>
-          <Animatable.View animation="fadeInDownBig" style={styles.box}>
-            <Text style={styles.title}>Create Account</Text>
-            <Text style={styles.header}>Create your account. It's free and only takes a couple of minutes</Text>
-            <View style={styles.input}>
-              <TextInput
-                  style={styles.textInput}
-                  placeholder="Username"
-                  placeholderTextColor="#003f5c"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  onChangeText={(data)=>setUName(data)}
-              />
-            </View>
-            <View style={styles.input}>
-              <TextInput
-                  style={styles.textInput}
-                  placeholder="First Name"
-                  placeholderTextColor="#003f5c"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  onChangeText={(data)=>SetFname(data)}
-              />
-              <TextInput
-                  style={styles.textInput}
-                  placeholder="Last Name"
-                  placeholderTextColor="#003f5c"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  onChangeText={(data)=>SetLname(data)}
-              />
-            </View>
-            <View style={styles.input}>
-              <TextInput
-                  style={styles.textInput}
-                  placeholder="Email"
-                  keyboardType={"email-address"}
-                  placeholderTextColor="#003f5c"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  onChangeText={(data)=>SetEmail(data)}
-              />
-            </View>
-            <View style={styles.input}>
-              <TextInput
-                  style={styles.textInput}
-                  placeholder="Password"
-                  secureTextEntry={true}
-                  placeholderTextColor="#003f5c"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  onChangeText={(data)=>setPass(data)}
-              />
-            </View>
-            <View style={styles.input}>
-              <TextInput
-                  style={styles.textInput}
-                  placeholder="Verify Password"
-                  secureTextEntry={true}
-                  placeholderTextColor="#003f5c"
-                  autoCapitalize="none"
-                  autoCorrect={false}
-                  onChangeText={(data)=>setVpass(data)}
-              />
-            </View>
-            <View>
-              <TouchableOpacity onPress={()=>navigation.navigate("AccountDetailsScreen",{
-                                                                   email: Email,
-                                                                   username: username,
-                                                                   pass: pass,
-                                                                   firstname: fname,
-                                                                   lastname:lname,
-                                                                   vPass:vPass,
-               })}>
-                <LinearGradient colors={["#37c9fc", "#1498D5"]} style={styles.button}>
-                  <Text style={styles.buttonText}>Next</Text>
-                  <MaterialIcons name="chevron-right" color='white' size={20}/>
-                </LinearGradient>
-              </TouchableOpacity>
-            </View>
-          </Animatable.View>
-          <View style={styles.logIn}>
-            <Text style={styles.logText}>Already have an account?</Text>
-            <TouchableOpacity onPress={() => navigation.navigate("LoginScreen")}>
-              <Text style={styles.signIn}>Sign in</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/SignupBG.jpg")}
+        style={styles.image}
+        blurRadius={Platform.OS == "ios" ? 10 : 1}
+      >
+        <Animatable.View animation="fadeInDownBig" style={styles.box}>
+          <Text style={styles.title}>Create Account</Text>
+          <Text style={styles.header}>
+            Create your account. It's free and only takes a couple of minutes
+          </Text>
+          <View style={styles.input}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Username"
+              placeholderTextColor="#003f5c"
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={(data) => setUName(data)}
+            />
+          </View>
+          <View style={styles.input}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="First Name"
+              placeholderTextColor="#003f5c"
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={(data) => SetFname(data)}
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Last Name"
+              placeholderTextColor="#003f5c"
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={(data) => SetLname(data)}
+            />
+          </View>
+          <View style={styles.input}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Email"
+              keyboardType={"email-address"}
+              placeholderTextColor="#003f5c"
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={(data) => SetEmail(data)}
+            />
+          </View>
+          <View style={styles.input}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Password"
+              secureTextEntry={true}
+              placeholderTextColor="#003f5c"
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={(data) => setPass(data)}
+            />
+          </View>
+          <View style={styles.input}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Verify Password"
+              secureTextEntry={true}
+              placeholderTextColor="#003f5c"
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={(data) => setVpass(data)}
+            />
+          </View>
+          <View>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("AccountDetailsScreen", {
+                  email: Email,
+                  username: username,
+                  pass: pass,
+                  firstname: fname,
+                  lastname: lname,
+                  vPass: vPass,
+                })
+              }
+            >
+              <LinearGradient
+                colors={["#37c9fc", "#1498D5"]}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>Next</Text>
+                <MaterialIcons name="chevron-right" color="white" size={20} />
+              </LinearGradient>
             </TouchableOpacity>
           </View>
-        </ImageBackground>
-      </View>
+        </Animatable.View>
+        <View style={styles.logIn}>
+          <Text style={styles.logText}>Already have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("LoginScreen")}>
+            <Text style={styles.signIn}>Sign in</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  image:{
-    flex:1,
-    resizeMode:'cover',
-    justifyContent:'center'
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
   container: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
-  box:{
-    borderWidth:2,
-    borderColor:'#1498D5',
-    borderRadius:15,
-    backgroundColor:'white',
-    height:500,
-    width:350,
-    alignSelf:'center',
-    alignItems:'center'
+  box: {
+    borderWidth: 2,
+    borderColor: "#1498D5",
+    borderRadius: 15,
+    backgroundColor: "white",
+    height: 500,
+    width: 350,
+    alignSelf: "center",
+    alignItems: "center",
   },
-  title:{
+  title: {
     fontSize: 30,
     marginBottom: 10,
-    marginTop: 10
+    marginTop: 10,
   },
-  header:{
+  header: {
     fontSize: 11,
     marginBottom: 40,
-    color:'gray'
+    color: "gray",
   },
-  input:{
-    width:'80%',
+  input: {
+    width: "80%",
     flexDirection: "row",
     marginTop: 10,
     borderBottomWidth: 1,
@@ -181,35 +184,35 @@ const styles = StyleSheet.create({
     paddingBottom: 2,
     marginBottom: 30,
   },
-  textInput:{
+  textInput: {
     flex: 1,
     marginTop: Platform.OS === "ios" ? 0 : -12,
     paddingLeft: 10,
     color: "black",
   },
-  button:{
-    marginTop:10,
+  button: {
+    marginTop: 10,
     width: 100,
     height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 20,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    paddingRight:5
+    color: "white",
+    fontWeight: "bold",
+    paddingRight: 5,
   },
-  logIn:{
-    marginTop:20
+  logIn: {
+    marginTop: 20,
   },
-  logText:{
-    alignSelf:'center',
+  logText: {
+    alignSelf: "center",
   },
-  signIn:{
-    marginTop:10,
-    alignSelf:'center',
-    fontSize:20,
+  signIn: {
+    marginTop: 10,
+    alignSelf: "center",
+    fontSize: 20,
   },
 });
